cd2fbc7563006a7b8e750aa7ba853275bc5a803d
jesly <jeslyanas@gmail.com>
2025-09-26 00:27:59 +0530

    Update url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://be-project-1-git-main-jeslys-projects-fb6c4fc3.vercel.app/api",
+  baseURL: "https://be-project-1-sfmn.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
