a3b863bd087a000acddb0baa5ebd85438c26942b
jesly <jeslyanas@gmail.com>
2025-09-25 23:56:52 +0530

    Updated backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://be-project-1-git-main-jeslys-projects-fb6c4fc3.vercel.app/api", 
 });
 
 API.interceptors.request.use((req) => {
