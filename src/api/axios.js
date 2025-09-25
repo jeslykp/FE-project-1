8b14c21ea3f144621520e3e001b36f8701936f6b
jesly <jeslyanas@gmail.com>
2025-09-26 00:19:36 +0530

    Updated backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://be-project-1-git-main-jeslys-projects-fb6c4fc3.vercel.app/api", 
+  baseURL: "https://be-project-1-git-main-jeslys-projects-fb6c4fc3.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) req.headers['Authorization'] = `Bearer ${token}`;
   return req;
 });
 
